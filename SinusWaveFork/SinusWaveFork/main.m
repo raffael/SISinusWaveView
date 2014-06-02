963f4098209e5599b790e7e610f4056926160a45
Ben Martin <ben@martinben.com>
2014-06-01 21:10:42 -0700

    Added .xcodeproj and .gitignore

---
+++ SinusWaveFork/SinusWaveFork/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SinusWaveFork
+//
+//  Created by Benjamin Martin on 6/1/14.
+//  Copyright (c) 2014 Benjamin Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
